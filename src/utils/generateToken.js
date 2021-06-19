0617f61439d67008b193bae52080ae5f67be5faf
Adetayo <akinsanyaadetayo7@gmail.com>
2021-06-19 17:41:01 +0100

    Implement Signup

---
+++ src/utils/generateToken.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken")
+
+
+const generateToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRATION
+  })
+}
+
+module.exports = generateToken
