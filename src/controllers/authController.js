e1bed047d475b82234b89404c5f233c328d72fe6
Adetayo <akinsanyaadetayo7@gmail.com>
2021-06-19 15:53:12 +0100

    Chore: Create cookie on signup
    Signed-off-by: Adetayo <akinsanyaadetayo7@gmail.com>

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,37 @@
+const asyncHandler = require("../middlewares/asyncHandler");
+const User = require("../model/user");
+const createSendToken = require("../utils/createSendToken");
+const ErrorHandler = require("../utils/errorHandler");
 
 
+/**
+ * @desc Register User
+ * @route POST /api/v1/auth/register
+ * @access Public
+ */
 
 exports.signUp = asyncHandler(async(req, res, next) => {
+  const {firstName, lastName, email, password, passwordConfirm} = req.body 
   
+
+  const existingUser = await User.findOne({email})
+
+  if (existingUser) {
+    return next(
+      new ErrorHandler('User already exists, please create a new account', 404)
+    );
+  }
+
+  const newUser = await User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+    passwordConfirm,
+    role,
+    ...req.body
+  });
+
+  createSendToken(newUser, 201, res);
+
 })
--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,11 +1,13 @@
-class AppError extends Error {
+class ErrorHandler extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
 
-    Error.captureStackTrace(this, this.constructor);
+    if (process.env.NODE_ENV === production){
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
 
-module.exports = AppError;
+module.exports = ErrorHandler;
