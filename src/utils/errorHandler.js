3445bef7cc3a77cc182f713bee47d6c45e5af755
Adetayo <akinsanyaadetayo7@gmail.com>
2021-06-19 17:40:34 +0100

    Chore: finish signup
    Signed-off-by: Adetayo <akinsanyaadetayo7@gmail.com>

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -3,11 +3,8 @@ class ErrorHandler extends Error {
     super(message);
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-
-    if (process.env.NODE_ENV === 'production'){
-      Error.captureStackTrace(this, this.constructor);
     }
   }
-}
+
 
 module.exports = ErrorHandler;
