163e934db89230f86d9d842bda07f4316e16ac93
franeldev <franel4u@gmail.com>
2021-07-12 23:43:51 +0100

    adminsvc.js file created

--- src/services/adminsvc.js
+++ src/services/adminsvc.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("../middlewares/asyncHandler");
-const User = require("../models/user");
+const User = require("../models/admin");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
@@ -10,15 +10,6 @@ const secret = process.env.JWT_SECRET;
 */
 const adminSignup = async (userData, role, res) => {
   try {
-    // // Validate the username
-    // let userNameNotTaken = await validateUsername(userData.userName);
-    // if (!userNameNotTaken) {
-    //   return res.status(400).json({
-    //     message: "Username is already taken.",
-    //     success: false
-    //   });
-    // }
-    // Validate the email
     let emailNotRegistered = await validateEmail(userData.email);
     if (!emailNotRegistered) {
       return res.status(400).json({
@@ -38,7 +29,7 @@ const adminSignup = async (userData, role, res) => {
     });
     await newUser.save();
     return res.status(201).json({
-      message: "Hurray! now you are successfully registered. Please now login.",
+      message: "Hurray! now you are successfully registered as an Admin. Please now login.",
       success: true
     });
   } catch (err) {
@@ -49,102 +40,13 @@ const adminSignup = async (userData, role, res) => {
   }
 };
 
-
-/* 
-** @Desc Validate the "Organization" to be Registered
-*/
-const orgSignup = async (userData, role, res) => {
-  try {
-    // // Validate the username
-    // let userNameNotTaken = await validateUsername(userData.userName);
-    // if (!userNameNotTaken) {
-    //   return res.status(400).json({
-    //     message: "Username is already taken.",
-    //     success: false
-    //   });
-    // }
-    // Validate the email
-    let emailNotRegistered = await validateEmail(userData.email);
-    if (!emailNotRegistered) {
-      return res.status(400).json({
-        message: "Email is already registered.",
-        success: false
-      });
-    }
-
-    // Get the hashed password
-    const password = await bcrypt.hash(userData.password, 10);
-    // create a new user
-    const newUser  = new User({
-      ...userData,
-      password,
-      role,
-      isAdmin: 0
-    });
-    await newUser.save();
-    return res.status(201).json({
-      message: "Hurray! now you are successfully registered. Please now login.",
-      success: true
-    });
-  } catch (err) {
-    return res.status(500).json({
-      message: "Unable to create your account.",
-      success: false
-    });
-  }
-};
-
-
-/* 
-** @Desc Validate the "Ngo" to be Registered
-*/
-const ngoSignup = async (userData, role, res) => {
-  try {
-    // // Validate the username
-    // let userNameNotTaken = await validateUsername(userData.userName);
-    // if (!userNameNotTaken) {
-    //   return res.status(400).json({
-    //     message: "Username is already taken.",
-    //     success: false
-    //   });
-    // }
-    // Validate the email
-    let emailNotRegistered = await validateEmail(userData.email);
-    if (!emailNotRegistered) {
-      return res.status(400).json({
-        message: "Email is already registered.",
-        success: false
-      });
-    }
-
-    // Get the hashed password
-    const password = await bcrypt.hash(userData.password, 10);
-    // create a new user
-    const newUser  = new User({
-      ...userData,
-      password,
-      role,
-      isAdmin: 0
-    });
-    await newUser.save();
-    return res.status(201).json({
-      message: "Hurray! now you are successfully registered. Please now login.",
-      success: true
-    });
-  } catch (err) {
-    return res.status(500).json({
-      message: "Unable to create your account.",
-      success: false
-    });
-  }
-};
 
 
 /**
   * Handle login credentials 
  */
 
- const userLogin = asyncHandler(async (req, role, res) => {
+ const adminLogin = asyncHandler(async (req, role, res) => {
   let { email, password } = req;
   if(!email || !password) {
     return res.status(400).json({
@@ -175,7 +77,6 @@ const ngoSignup = async (userData, role, res) => {
     let token = jwt.sign({ 
         user_id: user._id,
         role: user.role,
-        userName: user.userName,
         email: user.email 
       }, 
       secret,
@@ -183,7 +84,6 @@ const ngoSignup = async (userData, role, res) => {
       );
 
       let result = {
-        userName: user.userName,
         role: user.role,
         email: user.email,
         token,
@@ -203,10 +103,6 @@ const ngoSignup = async (userData, role, res) => {
     }
 });
 
-const validateUsername = async userName => {
-  let user = await User.findOne({ userName });
-  return user ? false : true;
-}
 
 const validateEmail = async email => {
   let user = await User.findOne({ email });
@@ -216,7 +112,5 @@ const validateEmail = async email => {
 
 module.exports = {
   adminSignup,
-  orgSignup,
-  ngoSignup, 
-  userLogin 
+  adminLogin 
 };
